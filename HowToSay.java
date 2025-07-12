2a7db711da0d3aa95876085fd66661ed7005f29d
Murphy Amos <46387288+MurphyAmos@users.noreply.github.com>
2025-07-11 20:39:00 -0500

    Add files via upload

---
+++ HowToSay.java
@@ -0,0 +1,77 @@
+import java.io.File;
+import java.util.*;
+import java.io.FileNotFoundException;
+public class HowToSay{
+    public static boolean completeWhile = false; 
+    public static int binarySearch(String arr[], String word){
+        int left = 0;
+        int right = arr.length -1;
+        
+        //while not at the same spot 
+        while(left<=right){
+            //find middle
+            int mid = left + (right -left) / 2;
+            //if middle index is equal to woord
+            if(arr[mid].equalsIgnoreCase(word)){
+                //return mid
+                return mid;
+            }
+            //if mid is not the word
+            if (arr[mid].compareToIgnoreCase(word) < 0){
+                //left is equal to mid +1 (go righ on left end)                
+                left = mid + 1;
+            }else{
+               //else go to right end and go left 1
+                right = mid -1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        //basic input for word
+        Scanner input = new Scanner(System.in);
+        System.out.print("Word: ");
+        String word = input.next();
+       
+        // holder arryList
+        List<String> rec = new ArrayList<>();
+
+        try{
+            //try to find fill in folder
+            File filePath = new File("/home/murphy/Desktop/JavaStuff/Prononce/cmudict_0_6.txt");
+            //make scanner
+            Scanner reader = new Scanner(filePath);
+            String[] dataSplit = {};
+            //while reading through reader            
+            while(reader.hasNextLine()){
+                String wordData = reader.nextLine();
+                //make a readable array                
+                dataSplit = wordData.split("  ");
+                //add string objects into rec                
+                for(String i : dataSplit){
+                    rec.add(i);              
+                    //if the word matches print out the pronounciation and complete it 
+                    if(word.equalsIgnoreCase(dataSplit[0])){
+                        System.out.println(i);
+                        completeWhile = true;
+                    }
+                }         
+                
+            }
+            //make a string array object to turn rec into array
+            String[] arrayHolder = rec.toArray(new String[0]); 
+            //sort it
+            Arrays.sort(arrayHolder);
+            //call function print resuls where its located at      
+            System.out.println("Word is located at index: "+ binarySearch(arrayHolder, word));            
+            //if we havent fufilled our loop then print out the word isnt there
+            if(completeWhile !=true){
+                System.out.println("Cant find "+ word);
+            }
+        }catch (FileNotFoundException e){
+            System.out.print("Not There");
+        }
+        
+    }
+}
